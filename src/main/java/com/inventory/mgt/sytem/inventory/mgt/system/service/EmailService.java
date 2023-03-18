d5ca2725979be749bee97cb6d10e2f57cbe9e65e
Kenechukwu <kenechukwubanego@gmail.com>
2023-03-18 15:47:08 +0100

    Changed email comments

--- src/main/java/com/inventory/mgt/sytem/inventory/mgt/system/service/EmailService.java
+++ src/main/java/com/inventory/mgt/sytem/inventory/mgt/system/service/EmailService.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+/***
+ * This is a service that provides mail functionality
+ * ****/
 public interface EmailService {
 
     public  void sendMail(String toEmail, String subject, String body);
